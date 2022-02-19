d4889df92fb6393824611047289b0b030e3fc8b9
JuanMa <ingjuanmasuarez@gmail.com>
2022-02-19 12:35:56 -0500

    Reduccion del tamaño del search. Se agregan los iconos de redes sociales

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,15 @@ import logo1200 from '../assets/images/1200pxTranspKikeana15Oct20.png';
 import banner1_600 from '../assets/images/Banner1Small600.jpg';
 import banner1_800 from '../assets/images/Banner1Medium800.jpg';
 import banner1_1200 from '../assets/images/Banner1Large1200.jpg';
+
 import icon_menu from '../assets/icons/icon_menu.svg';
+import icon_instagram from '../assets/icons/icon_instagram.svg';
+import icon_facebook from '../assets/icons/icon_facebook.svg';
+import icon_twitter from '../assets/icons/icon_twitter.svg';
+import icon_pinterest from '../assets/icons/icon_pinterest.svg';
+import icon_tiktok from '../assets/icons/icon_tiktok.svg';
+import icon_whatsapp from '../assets/icons/icon_whatsapp.svg';
+import icon_messenger from '../assets/icons/icon_messenger.svg';
 
 const Home = () => {
   return (
@@ -16,7 +24,7 @@ const Home = () => {
             
             <div className = "header-container">
 
-                <img src = { icon_menu } alt="Icono de Menu Desplegable" className = "icon-menu" />
+                <img src = { icon_menu } alt = "Icono de Menu Desplegable" className = "icon-menu" />
                 
                 <picture className = "logo">
                     <source media = "(min-width:800px)" srcset = { logo1200 } />
@@ -29,6 +37,29 @@ const Home = () => {
                         <input type = "text" placeholder = "Buscar..." />
                         <span className = "search-icon">
                         </span>
+                    </div>
+                    <div className = "social-media-container">
+                        <a href = "https://www.instagram.com/KikeanaArt" target="_blank">
+                            <img src = { icon_instagram } alt = "Icono de nuestro Instagram" />
+                        </a>
+                        <a href = "https://www.facebook.com/KikeanaArt" target="_blank">
+                            <img src = { icon_facebook } alt = "Icono de nuestro Facebook" />
+                        </a>
+                        <a href = "https://twitter.com/KikeanaArt" target="_blank">
+                            <img src = { icon_twitter } alt = "Icono de nuestro Twitter" />
+                        </a>
+                        <a href = "https://co.pinterest.com/KikeanaArt" target="_blank">
+                            <img src = { icon_pinterest } alt = "Icono de nuestro Pinterest" />
+                        </a>
+                        <a href = "https://www.tiktok.com/@KikeanaArt" target="_blank">
+                            <img src = { icon_tiktok } alt = "Icono de nuestro Tiktok" />
+                        </a>
+                        <a href = "https://wa.me/message/UFD4QQZHUERLC1" target="_blank">
+                            <img src = { icon_whatsapp } alt = "Icono de nuestro WhatsApp" />
+                        </a>
+                        <a href = "https://m.me/KikeanaArt" target="_blank">
+                            <img src = { icon_messenger } alt = "Icono de nuestro Messenger" />
+                        </a>
                     </div>
                 </section>
 
